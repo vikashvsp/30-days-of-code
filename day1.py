305c89a2f42b6855d6787d5af2320923a82af500
vikash kumar <vkvikashkumar987@gmail.com>
2021-06-29 10:22:22 +0530

    update

--- day1.py
+++ day1.py
@@ -1,3 +1,11 @@
-inputString=input()
-print("Hello, World.")
-print(inputString)
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+integer=int(input())
+double=float(input())
+string=str(input())
+
+print(i+integer)
+print(d+double)
+print(s+string)
--- day2.py
+++ day2.py
@@ -1,12 +1,17 @@
-i = 4
-d = 4.0
-s = 'HackerRank '
+import math
+import os
+import random
+import re
+import sys
 
-integer=int(input())
-double=float(input())
-string=str(input())
+def solve(meal_cost, tip_percent, tax_percent):
+    print(round(meal_cost+(meal_cost*tip_percent/100)+(meal_cost*tax_percent/100)))
+    
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
 
-print(i+integer)
-print(d+double)
-print(s+string)
+    tip_percent = int(input().strip())
 
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
